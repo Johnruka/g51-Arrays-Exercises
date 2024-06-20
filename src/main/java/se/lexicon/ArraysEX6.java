9c41d6a838cb296f489a21de7f15b0d3edca434d
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-20 10:26:02 +0200

    Created

---
+++ src/main/java/se/lexicon/ArraysEX6.java
@@ -0,0 +1,18 @@
+package se.lexicon;
+
+import java.text.DecimalFormat;
+
+public class ArraysEX6 {
+
+    public static void main(String[] args) {
+        double[] forAverage = {34, 15, 73, 16, 4, 155};
+        double total = 0;
+
+        for (double v : forAverage) {
+            total += v;
+        }
+
+        DecimalFormat df = new DecimalFormat("###.#");
+        System.out.println("Average is: " + df.format(total / forAverage.length));
+    }
+}
